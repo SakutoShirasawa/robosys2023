07e79657d85d80128ac7b02a584740fad90bda10
Sakuto Shirasawa <ss3910business@gmail.com>
2023-11-02 09:40:34 +0900

    test update

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Sakuto Shirasawa
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,9 +9,18 @@ ng () {
 }
 
 res=0
-
+### 1/0 TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1]       || ng ${LINENO}
+[ "$[out]" = ""]  || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1]       || ng ${LINENO}
+[ "$[out]" = ""]  || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
